fbce191e6c33f756c348f64db59b7ba4b457aef5
Damien Mazeas <58029218+mazeasdamien@users.noreply.github.com>
2025-11-20 00:43:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-portfolio/',
+  base: '/',
   server: {
     host: true,
     port: 3000,
